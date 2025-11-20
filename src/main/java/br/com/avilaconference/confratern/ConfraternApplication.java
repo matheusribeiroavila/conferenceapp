bfcef4656162c331b7ea9696ec9d56a59c69578d
Matheus Ribeiro Ávila <ribeiroavilamatheus@gmail.com>
2025-11-20 15:20:21 -0300

    Project started god help me to understand back-end

---
+++ src/main/java/br/com/avilaconference/confratern/ConfraternApplication.java
@@ -0,0 +1,13 @@
+package br.com.avilaconference.confratern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfraternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfraternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/avilaconference/confratern/ConfraternApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avilaconference.confratern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfraternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
